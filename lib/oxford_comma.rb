7e309e2bef76f2b79389d035660072ad6cc05c0c
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-17 17:37:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length < 2 
+    my_string = array.join
+  elsif array.length == 2 
+    my_string = array.join(" and ")
+  else 
+    last_item = array.pop
+    my_string = array.join(", ")
+    total_string = my_string + ", and " + last_item
+  end
+  
 end
