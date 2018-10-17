5ac57e4b58a064b06dd4e1f58d4776b44b983f70
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-17 17:26:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.length < 2 
-    my_string = array.join(", ")
+    my_string = array.join
   elsif array.length == 2 
     my_string = array.join(" and ")
+  else 
+    
     
   end
   
